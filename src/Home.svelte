49a6bcaa99f85d5a24c22cd44439d6a486300ca6
Bobby Sills <backsideofdawn@gmail.com>
2025-12-07 14:24:02 +0700

    added personal description

--- src/Home.svelte
+++ src/Home.svelte
@@ -5,18 +5,40 @@
 <main>
   <Nav currentPage="home" />
 
-  <div class="container">
-    <img src="public/profile-photo.jpg" alt="Portrait of a 9th grader" />
-    <p>Welcome to my personal website.</p>
+  <div
+    style:display="flex"
+    style:margin="24px 0px"
+    style:gap="24px"
+    style:flex-wrap="wrap"
+  >
+    <img src="/profile-photo.jpg" alt="Portrait of a 9th grader" />
+    <div
+      style:display="flex"
+      style:flex-direction="column"
+      style:flex="1"
+      style:min-width="300px"
+    >
+      <h3>bobby sills</h3>
+      <span
+        >9th grade developer working out of vientiane, laos. just released an
+        app to learn to read lao. get it on <a
+          href="https://apps.apple.com/us/app/read-lao/id6749076081">ios</a
+        > and android (coming soon).
+      </span>
+      <span> </span>
+    </div>
   </div>
 </main>
 
 <style>
-  .container {
-    display: flex;
-  }
   img {
-    width: 20%;
+    border-radius: 10px;
+    max-width: 200px;
     height: auto;
+  }
+  h3 {
+    font-weight: normal;
+    font-style: italic;
+    color: var(--red);
   }
 </style>
