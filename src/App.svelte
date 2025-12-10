6e392629c30d11a06e781e15c490d3941d14c214
Bobby Sills <backsideofdawn@gmail.com>
2025-12-10 12:14:29 +0700

    added 404 page

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-  import Router from 'svelte-spa-router'
-  import Home from './Home.svelte'
-  import Blog from './Blog.svelte'
-  import BlogPost from './BlogPost.svelte'
-  import Books from './Books.svelte'
+  import Router from "svelte-spa-router";
+  import Home from "./Home.svelte";
+  import Blog from "./Blog.svelte";
+  import BlogPost from "./BlogPost.svelte";
+  import Books from "./Books.svelte";
+  import TelephoneGame from "./projects/TelephoneGame.svelte";
+  import NotFound from "./NotFound.svelte";
 
   const routes = {
-    '/': Home,
-    '/blog': Blog,
-    '/blog/:slug': BlogPost,
-    '/books': Books,
-  }
+    "/": Home,
+    "/blog": Blog,
+    "/blog/:slug": BlogPost,
+    "/books": Books,
+    "/telephone-game": TelephoneGame,
+    "*": NotFound,
+  };
 </script>
 
 <Router {routes} />
---
+++ src/NotFound.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Nav from "./lib/Nav.svelte";
+
+  $effect(() => {
+    document.title = "bobby sills | 404";
+  });
+</script>
+
+<main>
+  <Nav />
+
+  <div>
+    <h1 style:color="var(--red)" style:font-size="4rem" style:margin="0">
+      404
+    </h1>
+    <h2 style:color="var(--fg)" style:font-weight="normal">page not found</h2>
+    <p style:color="var(--fg2)" style:margin-top="20px">
+      sorry, you probably entered in a bad url.
+    </p>
+  </div>
+</main>
+
+<style>
+  main {
+    min-height: 50vh;
+  }
+</style>
